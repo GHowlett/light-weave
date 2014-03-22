04bca174a92bd328a706aa7a95bf8c3df0582476
LeonardMeagher2 <LeonardMeagher2@gmail.com>
2014-03-22 08:24:46 -0700

    upadte: main.js,index.html

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function() {
+	var tags = Tags.getTags();
+	$( "#tags" ).autocomplete({
+		source: tags
+	});
+});
+
+$("#submit").click(
+	function() {
+		console.log($("#tags").val())
+	}
+);
+
+var graph = new NodeGraph("body");
+$.getJSON("mockdataset.json",
+function(data){
+	var d = JSONParser.convertKeyValuePairsToNodesAndLinks(data);
+	for(var i = 0; i < d.nodes.length; i++){
+		graph.addNode(d.nodes[i].name);
+	}
+	for(var i = 0; i < d.links.length; i++){
+		console.log(d.links[i]);
+		graph.addLink(d.links[i].source,d.links[i].target);
+	}
+},
+function(err){
+	//tERROR!
+});
+
+console.log(graph);
