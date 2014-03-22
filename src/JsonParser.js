223078460d91440aad9b61923b25fa175d825a85
Dillon Eng <dillon@Dillons-MacBook-Pro-2.local>
2014-03-22 02:45:43 -0700

    adding JsonParser and accurate mockdataset.json

---
+++ src/JsonParser.js
@@ -0,0 +1,47 @@
+var JSONParser = {};
+
+JSONParser.convertKeyValuePairsToNodesAndLinks = function(jsonObject) {
+  var mapIndex = 0;
+  var nodes = [];
+  var nodeBackMap = {};
+  for (index in Object.keys(jsonObject)) {
+    var key = Object.keys(jsonObject)[index];
+    var vals = jsonObject[key];
+    for (var i = 0; i < vals.length; i++) {
+      var val = vals[i];
+      if (nodeBackMap[val] == null) {
+        nodes.push({name: val});
+        nodeBackMap[val] = mapIndex++;
+      }
+    }
+  }
+
+  var links = [];
+  var linkExistsMap = {};
+  for (index in Object.keys(jsonObject)) {
+    var key = Object.keys(jsonObject)[index];
+    var vals = jsonObject[key];
+
+    for (var i = 0; i < vals.length; i++) {
+
+      var val = vals[i];
+      var srcNodeIndex = nodeBackMap[val];
+
+      for(var j = i + 1; j < vals.length; j++) {
+
+        var val2 = vals[j];
+        var dstNodeIndex = nodeBackMap[val2];
+        var linkMapKey = srcNodeIndex + "-" + dstNodeIndex;
+
+        if (linkExistsMap[linkMapKey] == null) {
+          linkExistsMap[linkMapKey] = true;
+          var toPush = {source: srcNodeIndex, target: dstNodeIndex};
+          links.push(toPush);
+        }
+
+      }
+    }
+  }
+
+  return {nodes: nodes, links: links};
+}
