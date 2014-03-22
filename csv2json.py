7a06cb6557724bcd8858e466c4ea73d246103f41
Alexander <alexandermichaelmiranda@gmail.com>
2014-03-22 11:21:10 -0700

    Corrected data in topical_list.json and added if conditions to csv2json.py

--- csv2json.py
+++ csv2json.py
@@ -5,7 +5,11 @@ from sys import argv
 csvfile = open(argv[1], 'r')
 jsonfile = open(argv[2], 'w')
 
-fieldnames = ("Verses", "Theme", "Emotion", "Images", "Genre", "Character", "Location", "Gender", "Question about God's Presence", "Answers")
+if (argv[1] == "data/pres_of_god.csv"):
+	fieldnames = ("Verses", "Theme", "Emotion", "Images", "Genre", "Character", "Location", "Gender", "Question about God's Presence", "Answers")
+
+if (argv[1] == "data/topical_list.csv"):
+	fieldnames = ("Topic", "OSIS", "Quality Score")
 
 reader = csv.DictReader( csvfile, fieldnames)
 firstline = True
