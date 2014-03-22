da07c93dd507faf50074ad4ed5f8cda15872f82a
Alexander <alexandermichaelmiranda@gmail.com>
2014-03-22 10:36:34 -0700

    Corrected bug in script that ignores first line of csv file

--- csv2json.py
+++ csv2json.py
@@ -7,7 +7,11 @@ jsonfile = open('data.json', 'w')
 fieldnames = ("Verses", "Theme", "Emotion", "Images", "Genre", "Character", "Location", "Gender", "Question about God's Presence", "Answers")
 
 reader = csv.DictReader( csvfile, fieldnames)
+firstline = True
 for row in reader:
+	if firstline:
+		firstline = False
+		continue
 	json.dump(row, jsonfile)
 	jsonfile.write('\n')
 
